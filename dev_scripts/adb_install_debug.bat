31da788140ca357fdebcd088d8ac76650f382912
bandeapart <dont.try.email.me@commit>
2024-11-13 18:20:27 +0800

    update dev scripts

--- dev_scripts/adb_install_debug.bat
+++ dev_scripts/adb_install_debug.bat
@@ -1,3 +1,4 @@
 @echo off
 cd ../
 adb -s %1 install app\build\outputs\apk\debug\app-debug.apk
+adb shell monkey -p com.catpuppyapp.puppygit.play.pro -c android.intent.category.LAUNCHER 1
--- dev_scripts/adb_install_release.bat
+++ dev_scripts/adb_install_release.bat
@@ -1,3 +1,4 @@
 @echo off
 cd ../
 adb -s %1 install app\build\outputs\apk\release\app-release.apk
+adb shell monkey -p com.catpuppyapp.puppygit.play.pro -c android.intent.category.LAUNCHER 1
