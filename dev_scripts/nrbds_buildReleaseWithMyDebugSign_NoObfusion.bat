bf31ebe4bbfa79c8363f6d9188cdc43d2511a08e
bandeapart <dont.try.email.me@commit>
2024-11-18 13:05:04 +0800

    add script

---
+++ dev_scripts/nrbds_buildReleaseWithMyDebugSign_NoObfusion.bat
@@ -0,0 +1,5 @@
+@echo off
+cd ../
+./gradlew assembleRelease -Pandroid.injected.signing.store.file=%USERPROFILE%/.myandroid/mydebugkey-android.jks -Pandroid.injected.signing.store.password=android -Pandroid.injected.signing.key.alias=android -Pandroid.injected.signing.key.password=android -PminifyEnabled=false
+
+pause
