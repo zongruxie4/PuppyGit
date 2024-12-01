71e6c8e831edacd1e82e56560d1b1ebfaa1d06bb
bandeapart <dont.try.email.me@commit>
2024-12-02 00:11:46 +0800

    update `ReflogListScreen`: show ref name on title info dialog

--- app/src/main/java/com/catpuppyapp/puppygit/screen/ReflogListScreen.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/screen/ReflogListScreen.kt
@@ -2,6 +2,7 @@ package com.catpuppyapp.puppygit.screen
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -12,6 +13,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -261,7 +263,15 @@ fun ReflogListScreen(
 
     val showTitleInfoDialog = rememberSaveable { mutableStateOf(false)}
     if(showTitleInfoDialog.value) {
-        RepoInfoDialog(curRepo.value, showTitleInfoDialog)
+        RepoInfoDialog(
+            prependContent = {
+                Row {
+                    Text(stringResource(R.string.reference) + ": " + refName.value)
+                }
+            },
+            curRepo = curRepo.value,
+            showTitleInfoDialog = showTitleInfoDialog
+        )
     }
 
     val dropDownMenuExpendState = rememberSaveable { mutableStateOf(false)}
