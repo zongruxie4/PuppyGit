e74d46c4da4cbb81460bcfef2f6940bd16d1ec67
bandeapart <dont.try.email.me@commit>
2024-12-01 23:06:20 +0800

    `BranchItem`: make current branch name highlighting

--- app/src/main/java/com/catpuppyapp/puppygit/compose/BranchItem.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/BranchItem.kt
@@ -7,21 +7,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.catpuppyapp.puppygit.play.pro.R
 import com.catpuppyapp.puppygit.git.BranchNameAndTypeDto
+import com.catpuppyapp.puppygit.play.pro.R
+import com.catpuppyapp.puppygit.style.MyStyleKt
 import com.catpuppyapp.puppygit.utils.UIHelper
 import com.catpuppyapp.puppygit.utils.doJobThenOffLoading
 import com.catpuppyapp.puppygit.utils.state.CustomStateSaveable
@@ -102,6 +103,7 @@ fun BranchItem(
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
                 fontWeight = if(thisObj.isCurrent) FontWeight.ExtraBold else FontWeight.Light,
+                color = if(thisObj.isCurrent) MyStyleKt.TextColor.highlighting_green else Color.Unspecified
             )
         }
 
