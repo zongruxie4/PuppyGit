58e197ac1f0962de92b8c11021af966d33b7ff09
bandeapart <invalidmail@noreply.581328804>
2024-12-12 09:33:28 +0800

    update bundled cert

--- app/src/main/java/com/catpuppyapp/puppygit/utils/cert/CertMan.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/utils/cert/CertMan.kt
@@ -16,7 +16,7 @@ object CertMan {
     private val TAG = "CertMan"
 
     //若更新证书，应更新此值
-    const val currentVersion = 2
+    const val currentVersion = 3
 
     private val certRawId = R.raw.cert_bundle_pem
 
