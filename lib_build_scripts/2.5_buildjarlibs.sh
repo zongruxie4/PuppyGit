5c672f7c1bf1ca4f556aad84fa69c68b129acf67
bandeapart <dont.try.email.me@commit>
2024-12-13 12:02:04 +0800

    remove build git24j in github workflow file (yml)

--- lib_build_scripts/2.5_buildjarlibs.sh
+++ lib_build_scripts/2.5_buildjarlibs.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# stop if has err
-set -e
-# download src and create necessary dirs by `downloadsrc.sh`
-export build_root=~/puppylibsbuild
-export build_out=$build_root/out
-export build_src=$build_root/src
-mkdir -p $build_out
-mkdir -p $build_src
 
 echo "no jar need build for now"
 
