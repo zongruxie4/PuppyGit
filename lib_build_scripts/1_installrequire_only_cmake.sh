96d761f6ae8a1bbaa3f3a741378fc42eee20e6eb
bandeapart <dont.try.email.me@commit>
2024-12-14 13:01:40 +0800

    update test script

--- lib_build_scripts/1_installrequire_only_cmake.sh
+++ lib_build_scripts/1_installrequire_only_cmake.sh
@@ -12,16 +12,6 @@ export build_root=~/puppylibsbuild
 mkdir -p $build_root
 cd $build_root
 
-export ndk_filename="android-ndk-r26d-linux"
-echo "Downloading: ndk"
-curl -L -o "${build_root}/$ndk_filename.zip" "https://dl.google.com/android/repository/${ndk_filename}.zip"
-export ANDROID_NDK_ROOT=$build_root/android-ndk
-mkdir -p $ANDROID_NDK_ROOT
-echo "Extracting: ndk"
-# -q : only show msg when err
-unzip -q "$ndk_filename.zip"
-mv android-ndk-r26d/* $ANDROID_NDK_ROOT
-
 echo "Downloading: Android SDK"
 # ANDROID_HOME is android sdk root, is sdk root, not ndk root
 export ANDROID_HOME=$build_root/android-sdk
