b9bbdf3dbf39c302b715d33b3d26c21e980014c7
bandeapart <dont.try.email.me@commit>
2024-12-14 13:39:21 +0800

    update script

--- lib_build_scripts/1_installrequire.sh
+++ lib_build_scripts/1_installrequire.sh
@@ -32,10 +32,15 @@ unzip cmdline-tools.zip -d $ANDROID_HOME/
 echo "install cmake by Android sdkmanager"
 yes | $ANDROID_HOME/cmdline-tools/bin/sdkmanager --install "cmake;$CMAKE_VERSION" --sdk_root=$ANDROID_HOME
 $ANDROID_HOME/cmdline-tools/bin/sdkmanager --list --sdk_root=$ANDROID_HOME
-export CMAKE_PATH=$ANDROID_HOME/cmake/$CMAKE_VERSION/bin/cmake
+# cmake root dir
+export CMAKE_DIR=$ANDROID_HOME/cmake/$CMAKE_VERSION
+# cmake 可执行文件路径
+export CMAKE_PATH=$CMAKE_DIR/bin/cmake
+echo "set cmake.dir to local.properties for gradle"
 # 设置 cmake.dir 以使 github workflow 使用gradle构建apk的时候能找到我们指定版本的cmake
 # -e是为了能输出换行符\n
-echo -e "\ncmake.dir=$ANDROID_HOME/cmake/$CMAKE_VERSION" >> $GITHUB_WORKSPACE/local.properties
+echo -e "\ncmake.dir=$CMAKE_DIR" >> $GITHUB_WORKSPACE/local.properties
+echo "print cmake version"
 $CMAKE_PATH --version
 
-echo "Installation complete."
+echo "Installation complete"
