980b272a7714e2f5e24f87282a5c718a86e03fc3
bandeapart <invalidmain@invalidmail12334556.com>
2025-01-20 17:24:06 +0800

    add another ClickableText compose

--- app/src/main/java/com/catpuppyapp/puppygit/compose/ClickableText.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/ClickableText.kt
@@ -3,6 +3,7 @@ package com.catpuppyapp.puppygit.compose
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.sp
@@ -21,5 +22,26 @@ fun ClickableText(text:String, onClick:()->Unit) {
         modifier = MyStyleKt.ClickableText.modifierNoPadding.clickable {
             onClick()
         },
+    )
+}
+
+
+@Composable
+fun ClickableText(
+    text:String,
+    maxLines:Int = Int.MAX_VALUE,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    fontWeight: FontWeight? = null,
+    modifier: Modifier
+) {
+    Text(
+        text = text,
+        maxLines = maxLines,
+        overflow = overflow,
+        fontWeight = fontWeight,
+        style = MyStyleKt.ClickableText.style,
+        color = MyStyleKt.ClickableText.color,
+        fontSize = 16.sp,
+        modifier = modifier,
     )
 }
