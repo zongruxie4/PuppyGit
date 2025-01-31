76a8d75f9071e433a560199415c5550e963f4468
bandeapart <dont.try.email.me@commit>
2025-01-31 10:41:12 +0800

    调整AppItem样式，带onClick的条目加了个图标

--- app/src/main/java/com/catpuppyapp/puppygit/compose/AppItem.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/AppItem.kt
@@ -10,20 +10,27 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toBitmap
 import com.catpuppyapp.puppygit.dto.AppInfo
+import com.catpuppyapp.puppygit.play.pro.R
 
 
 @Composable
 fun AppItem(
     appInfo:AppInfo,
     trailIcon: (@Composable BoxScope.(initModifier:Modifier)->Unit)? = null,
+
+    // item onClick，不是尾部图标的onClick
     onClick:((AppInfo) -> Unit)?
 ) {
 
@@ -32,10 +39,10 @@ fun AppItem(
             if(onClick == null) Modifier else Modifier.clickable { onClick(appInfo) }
         ),
     ) {
-        val iconModifier = if(trailIcon == null) Modifier.fillMaxWidth() else Modifier.fillMaxWidth(.9f)
+        val trailIconModifier = if(trailIcon == null) Modifier.fillMaxWidth() else Modifier.fillMaxWidth(.9f)
 
         Row(
-            modifier = iconModifier.align(Alignment.CenterStart)
+            modifier = trailIconModifier.align(Alignment.CenterStart)
         ) {
 
             Image(
@@ -49,7 +56,19 @@ fun AppItem(
             Column(
                 verticalArrangement = Arrangement.Center,
             ) {
-                Text(text = appInfo.appName)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = appInfo.appName)
+
+                    if(onClick != null) {
+                        Icon(
+                            modifier = Modifier.size(18.dp),
+                            imageVector = Icons.Outlined.Settings,
+                            contentDescription = stringResource(R.string.settings)
+                        )
+                    }
+                }
 
                 Text(text = appInfo.packageName, style = androidx.compose.material3.MaterialTheme.typography.bodySmall)
             }
