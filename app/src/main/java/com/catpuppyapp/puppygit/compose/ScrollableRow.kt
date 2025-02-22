35d4d8859f03bb6027b4e2b60e2ccecc5bb29f0c
bandeapart <dont.try.email.me@commit>
2025-02-22 13:46:13 +0800

    update ScrollableRow

--- app/src/main/java/com/catpuppyapp/puppygit/compose/ScrollableRow.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/ScrollableRow.kt
@@ -1,6 +1,7 @@
 package com.catpuppyapp.puppygit.compose
 
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
@@ -8,9 +9,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
-fun ScrollableRow(modifier: Modifier=Modifier,content:@Composable ()->Unit) {
-    Row(modifier = modifier.horizontalScroll(rememberScrollState()),
-        verticalAlignment = Alignment.CenterVertically
+fun ScrollableRow(
+    modifier: Modifier=Modifier,
+    horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
+    verticalAlignment: Alignment.Vertical = Alignment.Top,
+    content:@Composable ()->Unit
+) {
+    Row(
+        modifier = modifier.horizontalScroll(rememberScrollState()),
+        horizontalArrangement = horizontalArrangement,
+        verticalAlignment = verticalAlignment
     ) {
         content()
     }
