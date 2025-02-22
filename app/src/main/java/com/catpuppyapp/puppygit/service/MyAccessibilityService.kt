bc6e689237437746181b5bbcc88bb53dabdd7c49
bandeapart <dont.try.email.me@commit>
2025-02-22 11:16:05 +0800

    更新 MyAccessibilityService: 添加 com.oplus.notificationmanager 到忽略列表

--- app/src/main/java/com/catpuppyapp/puppygit/service/MyAccessibilityService.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/service/MyAccessibilityService.kt
@@ -35,6 +35,8 @@ class MyAccessibilityService: AccessibilityService() {
         private val ignorePackageNames = listOf<String>(
             "com.android.systemui",  //通知栏或全面屏手势之类的
             "android",  //切换输入法的弹窗，有的系统（例如原生）会是这个包名，有的不是（例如国产color os）
+            "com.oplus.notificationmanager",  //oneplus等oppo系系统，安装app，初次启动，弹窗，询问“是否允许当前app显示通知”
+
         )
 
 
