51b818031f964e82e7acc5357b9a91d6d39f7990
bandeapart <dont.try.email.me@commit>
2025-03-10 15:10:15 +0800

    update FilesInnerPage: make BottomBar number onclick dialog information simple

--- app/src/main/java/com/catpuppyapp/puppygit/screen/content/homescreen/innerpage/FilesInnerPage.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/screen/content/homescreen/innerpage/FilesInnerPage.kt
@@ -2189,7 +2189,10 @@ fun FilesInnerPage(
         SelectedItemDialog(
 //            detailStr = selectedItemsShortDetailsStr.value,
             selectedItems = selectedItems.value,
-            formatter = {"${it.name}, ${if(it.isDir) "dir" else "file"}, ${it.fullPath}"},
+            //这个太长，废弃
+//            formatter = {"${it.name}, ${if(it.isDir) "dir" else "file"}, ${it.fullPath}"},
+            // 这个简洁，示例： "File: abc.txt"，"Folder: 123"
+            formatter = {"${activityContext.getString(if(it.isDir) R.string.folder else R.string.file)}: ${it.name}"},
             switchItemSelected = switchItemSelected,
             clearAll = {selectedItems.value.clear()},
 
