a9a493e67cff6b15ccea7bfed067aa42b89451f3
bandeapart <dont.try.email.me@commit>
2025-03-13 10:55:30 +0800

    修改 FilterTextField ： 默认显示clear

--- app/src/main/java/com/catpuppyapp/puppygit/compose/FilterTextField.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/FilterTextField.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
@@ -11,8 +13,6 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
@@ -36,24 +36,22 @@ fun FilterTextField(
     loading:Boolean = false,
     placeholderText:String = stringResource(R.string.input_keyword),
     singleLine:Boolean = true,
-    trailingIconTooltipText: String="",
-    trailingIcon: ImageVector?=null,
-    trailingIconColor: Color?=null,
-    trailingIconDesc: String?=null,
-    trailingIconOnClick:(()->Unit)?=null,
+    trailingIcon: (@Composable ()->Unit)? = null,
+    showClear:Boolean = filterKeyWord.value.text.isNotEmpty(),
+    onClear:(()->Unit)? = {filterKeyWord.value = TextFieldValue("")},
     onValueChange:(newValue:TextFieldValue)->Unit = { filterKeyWord.value = it },
 ) {
-    val trailIcon:@Composable (() -> Unit)? = if(trailingIcon!=null){
+    //若尾部图标不为null，完全采用自定义选项；否则检查是否请求显示clear
+    val trailIcon:@Composable (() -> Unit)? = if(trailingIcon != null){
+        trailingIcon
+    }else if(showClear) {
         {
             LongPressAbleIconBtn(
-                tooltipText = trailingIconTooltipText,
-                icon = trailingIcon,
-                iconContentDesc = trailingIconDesc,
-                iconColor = trailingIconColor
+                tooltipText = stringResource(R.string.clear),
+                icon = Icons.Filled.Close,
             ) {
-                trailingIconOnClick?.invoke()
+                onClear?.invoke()
             }
-
         }
     }else {
         null
