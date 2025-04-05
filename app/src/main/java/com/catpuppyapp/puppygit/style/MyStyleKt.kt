d535ccf225838796e2eac3de043721e55d495822
bandeapart <dont.try.email.me@commit>
2025-04-05 15:15:44 +0800

    调整fab padding

--- app/src/main/java/com/catpuppyapp/puppygit/style/MyStyleKt.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/style/MyStyleKt.kt
@@ -129,7 +129,8 @@ class MyStyleKt{
 
         fun getFabModifier(isPortrait:Boolean):Modifier {
             //貌似Fab自带一点Padding，所以这里直接用BottomBar的高度即可，不需要再额外加padding
-            return addNavPaddingIfNeed(isPortrait, Modifier.padding(bottom = BottomBar.height))
+            // end 20dp 是为了避免浮动按钮盖住条目的三个点菜单按钮（例如Files页面，每个条目后面都有个3点菜单）
+            return addNavPaddingIfNeed(isPortrait, Modifier.padding(bottom = BottomBar.height, end = 20.dp))
         }
 
         //若是横屏，添加导航栏padding，不然浮动按钮会被导航栏盖住
