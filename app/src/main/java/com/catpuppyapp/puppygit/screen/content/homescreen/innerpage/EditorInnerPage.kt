daa5f220d7d5f34ccaff063f5afadc8151fdf847
bandeapart <dont.try.email.me@commit>
2025-04-25 22:11:30 +0800

    update Editor: 如果显示询问是否重载的弹窗，则不自动重载，让用户自己决定是否重载

--- app/src/main/java/com/catpuppyapp/puppygit/screen/content/homescreen/innerpage/EditorInnerPage.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/screen/content/homescreen/innerpage/EditorInnerPage.kt
@@ -1266,7 +1266,8 @@ fun EditorInnerPage(
 
     //按Home切换到别的app再返回，检查如果当前文件已保存（已编辑为假），则重载
     LifecycleEventEffect(Lifecycle.Event.ON_RESUME) {
-        if(isEdited.value.not()) {
+        //如果显示重载确认弹窗，则不自动重载；如果未显示重载确认弹窗且文件未编辑（换句话说：已成功保存），则自动重载
+        if(showBackFromExternalAppAskReloadDialog.value.not() && isEdited.value.not()) {
             reloadFile()
         }
     }
