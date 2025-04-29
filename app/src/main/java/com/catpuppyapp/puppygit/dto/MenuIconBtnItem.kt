d9b1d7ca96c836805ee3b65cfc150a82eca248c1
bandeapart <dont.try.email.me@commit>
2025-04-29 18:08:40 +0800

    更新MenuIconBtnItem

--- app/src/main/java/com/catpuppyapp/puppygit/dto/MenuIconBtnItem.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/dto/MenuIconBtnItem.kt
@@ -2,11 +2,11 @@ package com.catpuppyapp.puppygit.dto
 
 import androidx.compose.ui.graphics.vector.ImageVector
 
-data class MenuIconBtnItem (
+data class MenuIconBtnItem<T> (
     val icon:ImageVector,
     val text:String,
     val desc:String, // for accessbility
-    val enabled:()->Boolean,
-    val visible:()->Boolean,
-    val onClick:()->Unit,
+    val enabled:(T)->Boolean,
+    val visible:(T)->Boolean,
+    val onClick:(T)->Unit,
 )
