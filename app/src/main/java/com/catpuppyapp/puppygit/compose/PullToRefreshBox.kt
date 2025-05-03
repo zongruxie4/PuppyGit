545517a940571bd955467bf57f90e02c202deec9
bandeapart <dont.try.email.me@commit>
2025-05-03 13:26:05 +0800

    添加下拉刷新组件

---
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/PullToRefreshBox.kt
@@ -0,0 +1,92 @@
+package com.catpuppyapp.puppygit.compose
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
+import androidx.compose.material3.pulltorefresh.PullToRefreshState
+import androidx.compose.material3.pulltorefresh.pullToRefreshIndicator
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.catpuppyapp.puppygit.play.pro.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PullToRefreshBox(
+    isRefreshing: Boolean,
+    onRefresh: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable ()->Unit,
+) {
+    val state = rememberPullToRefreshState()
+
+    PullToRefreshBox(
+        isRefreshing = isRefreshing,
+        onRefresh = onRefresh,
+        modifier = modifier,
+        state = state,
+        indicator = {
+            MyCustomIndicator(
+                state = state,
+                isRefreshing = isRefreshing,
+                modifier = Modifier.align(Alignment.TopCenter)
+            )
+        }
+    ) {
+        content()
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun MyCustomIndicator(
+    state: PullToRefreshState,
+    isRefreshing: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier.pullToRefreshIndicator(
+            state = state,
+            isRefreshing = isRefreshing,
+            containerColor = PullToRefreshDefaults.containerColor,
+        ),
+        contentAlignment = Alignment.Center
+    ) {
+        Crossfade(
+            targetState = isRefreshing,
+            animationSpec = tween(),
+            modifier = Modifier.align(Alignment.Center)
+        ) { refreshing ->
+            if (refreshing) {
+                CircularProgressIndicator()
+            } else {
+                val distanceFraction = { state.distanceFraction.coerceIn(0f, 1f) }
+                Icon(
+                    imageVector = Icons.Filled.Refresh,
+                    contentDescription = stringResource(R.string.refresh),
+                    modifier = Modifier
+                        .size(18.dp)
+                        .graphicsLayer {
+                            val progress = distanceFraction()
+                            this.alpha = progress
+                            this.scaleX = progress
+                            this.scaleY = progress
+                        }
+                )
+            }
+        }
+    }
+}
