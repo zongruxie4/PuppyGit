36f23390a6679b48f6662fcc861b4e7b1c22393d
bandeapart <dont.try.email.me@commit>
2025-05-14 22:54:04 +0800

    Editor: 调patch mode颜色，和diff screen色调匹配

--- app/src/main/java/com/catpuppyapp/puppygit/utils/PatchUtil.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/utils/PatchUtil.kt
@@ -3,14 +3,14 @@ package com.catpuppyapp.puppygit.utils
 import androidx.compose.ui.graphics.Color
 
 object PatchUtil {
-    private val color_fileHeader_dark = Color(0xFF4D4414)
-    private val color_fileHeader = Color(0xFFC7BB81)
-    private val color_add_dark = Color(0xFF1B5E20)
-    private val color_add = Color(0xFF81C784)
-    private val color_del_dark = Color(0xFF484848)
-    private val color_del = Color(0xFFA2A2A2)
-    private val color_hunkHeader_dark = Color(0xFF01579B)
-    private val color_hunkHeader = Color(0xFF81B1C7)
+    private val color_fileHeader_dark = Color(0xAB4D4414)
+    private val color_fileHeader = Color(0xADC7BB81)
+    private val color_add_dark = Color(0x9E1B5E20)
+    private val color_add = Color(0xB481C784)
+    private val color_del_dark = Color(0xAD6B3E3E)
+    private val color_del = Color(0xA9B04242)
+    private val color_hunkHeader_dark = Color(0xB401579B)
+    private val color_hunkHeader = Color(0xB281B1C7)
 
     // 判断一段字符串有没有可能是 patch文件的内容。（也有叫diff文件的，总之就是git 导出的patch）
     fun maybeIsPatchLine(str:String) = isFileHeader(str) || isAdd(str) || isDelete(str) || isHunkHeader(str);
