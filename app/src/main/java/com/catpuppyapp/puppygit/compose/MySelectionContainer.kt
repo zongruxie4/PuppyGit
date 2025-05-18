a95bb7adb89667010f573342d19e8c5673c35f05
bandeapart <dont.try.email.me@commit>
2025-05-18 16:37:04 +0800

    用DisableSelection包MySelectionContainer

--- app/src/main/java/com/catpuppyapp/puppygit/compose/MySelectionContainer.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/MySelectionContainer.kt
@@ -1,5 +1,6 @@
 package com.catpuppyapp.puppygit.compose
 
+import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.runtime.Composable
@@ -15,9 +16,13 @@ fun MySelectionContainer(content:@Composable ()->Unit) {
     CompositionLocalProvider(
         LocalTextSelectionColors provides (if(inDarkMode) MyStyleKt.TextSelectionColor.customTextSelectionColors_darkMode else MyStyleKt.TextSelectionColor.customTextSelectionColors),
     ) {
-        //旧版m3，这个东西有bug，如果结束光标超过开始光标，会直接崩溃，但新版1.2.1已解决！目前用的版本是没问题的，不过好像只能复制，没有翻译之类的选项，可能还是有bug，或者我哪里设置的不对？
-        SelectionContainer{
-            content()
+        //旧版m3，这个东西有bug，如果结束光标超过开始光标，会直接崩溃，但新版1.2.1已解决！
+        // 目前用的版本是没问题的，不过好像只能复制，没有翻译之类的选项，可能还是有bug，或者我哪里设置的不对？（不是bug，就是故意的，想要更多选项需自己实现）
+        // 20250518更新：用禁用选择先包一下，不然可能有bug，例如页面用选择容器包围，内部弹窗再加选择容器且没禁用选择容器包裹，长按，app崩溃，这应该算bug，目前的解决方案就是用选择容器前先用禁用选择容器包一下
+        DisableSelection {
+            SelectionContainer{
+                content()
+            }
         }
 
     }
