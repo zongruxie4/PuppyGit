fdf36b7963b59b3d57f4a66973036cb42bde42ee
bandeapart <dont.try.email.me@commit>
2025-05-19 17:34:00 +0800

    调整输入主密码的页面的样式

--- app/src/main/java/com/catpuppyapp/puppygit/screen/RequireMasterPasswordScreen.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/screen/RequireMasterPasswordScreen.kt
@@ -1,6 +1,5 @@
 package com.catpuppyapp.puppygit.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,13 +7,15 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -25,7 +26,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -40,7 +40,6 @@ import com.catpuppyapp.puppygit.compose.PasswordTextFiled
 import com.catpuppyapp.puppygit.play.pro.R
 import com.catpuppyapp.puppygit.settings.SettingsUtil
 import com.catpuppyapp.puppygit.style.MyStyleKt
-import com.catpuppyapp.puppygit.ui.theme.Theme
 import com.catpuppyapp.puppygit.utils.AppModel
 import com.catpuppyapp.puppygit.utils.HashUtil
 import com.catpuppyapp.puppygit.utils.MyLog
@@ -119,76 +118,77 @@ fun RequireMasterPasswordScreen(
         }
     }
 
-    Column(
-        modifier = Modifier
-            //fillMaxSize 必须在最上面！要不然，文字不会显示在中间！
-            .fillMaxSize()
-            .systemBarsPadding()
-            .verticalScroll(rememberScrollState())
-            .then(
-                if(Theme.inDarkTheme) Modifier.background(Color.Black) else Modifier
-            )
-        ,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Spacer(modifier = Modifier.height(30.dp))
+    Scaffold { contentPadding ->
+        Column(
+            modifier = Modifier
+                //fillMaxSize 必须在最上面！要不然，文字不会显示在中间！
+                .fillMaxSize()
+                .padding(contentPadding)
+                .verticalScroll(rememberScrollState())
+                .imePadding()
+//            .padding(bottom = MyStyleKt.Padding.PageBottom)
+            ,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+//        Spacer(modifier = Modifier.height(30.dp))
 
-        AppIcon(modifier = Modifier.size(100.dp))
+            AppIcon(modifier = Modifier.size(100.dp))
 
-        Spacer(modifier = Modifier.height(30.dp))
+            Spacer(modifier = Modifier.height(30.dp))
 
 //        Row { Text(stringResource(R.string.master_password), fontSize = 30.sp) }
 //        Spacer(modifier = Modifier.height(60.dp))
 
 
-        PasswordTextFiled(
-            password = password,
-            passwordVisible = passwordVisible,
-            label = stringResource(R.string.master_password),
-            placeholder = stringResource(R.string.input_your_master_password),
-            focusRequest = focusRequest,
-            paddingValues = PaddingValues(start = 10.dp, end = 10.dp, top = 10.dp),
-            errMsg = errMsg,
-            enabled = loading.value.not(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Go),
-            keyboardActions = KeyboardActions(onGo = {
-                inputPassCallback()
-            })
-        )
-
-        Spacer(Modifier.height(10.dp))
-
-        ClickableText(
-            text = stringResource(R.string.i_forgot_my_master_password),
-            color = if(loading.value) UIHelper.getDisableTextColor() else MyStyleKt.ClickableText.color,
-            fontWeight = FontWeight.Light,
-            modifier = MyStyleKt.ClickableText.modifier.clickable(
+            PasswordTextFiled(
+                password = password,
+                passwordVisible = passwordVisible,
+                label = stringResource(R.string.master_password),
+                placeholder = stringResource(R.string.input_your_master_password),
+                focusRequest = focusRequest,
+                paddingValues = PaddingValues(start = 10.dp, end = 10.dp, top = 10.dp),
+                errMsg = errMsg,
                 enabled = loading.value.not(),
-                onClick = { showClearMasterPasswordDialog.value = true }
-            ),
-        )
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Go),
+                keyboardActions = KeyboardActions(onGo = {
+                    inputPassCallback()
+                })
+            )
 
-        Spacer(Modifier.height(20.dp))
-        // ok btn
-        Button(
-            enabled = loading.value.not(),
-            onClick = {
-                inputPassCallback()
+            Spacer(Modifier.height(10.dp))
+
+            ClickableText(
+                text = stringResource(R.string.i_forgot_my_master_password),
+                color = if(loading.value) UIHelper.getDisableTextColor() else MyStyleKt.ClickableText.color,
+                fontWeight = FontWeight.Light,
+                modifier = MyStyleKt.ClickableText.modifier.clickable(
+                    enabled = loading.value.not(),
+                    onClick = { showClearMasterPasswordDialog.value = true }
+                ),
+            )
+
+            Spacer(Modifier.height(20.dp))
+            // ok btn
+            Button(
+                enabled = loading.value.not(),
+                onClick = {
+                    inputPassCallback()
+                }
+            ) {
+                Text(stringResource(R.string.confirm))
             }
-        ) {
-            Text(stringResource(R.string.confirm))
-        }
 
-        Spacer(Modifier.height(20.dp))
+            Spacer(Modifier.height(20.dp))
 
-        if(loading.value) {
-            MySelectionContainer {
-                Text(loadingText.value, fontWeight = FontWeight.Light, color = MyStyleKt.TextColor.highlighting_green)
+            if(loading.value) {
+                MySelectionContainer {
+                    Text(loadingText.value, fontWeight = FontWeight.Light, color = MyStyleKt.TextColor.highlighting_green)
+                }
             }
-        }
 
-        Spacer(Modifier.height(100.dp))
+//        Spacer(Modifier.height(100.dp))
+        }
     }
 
     LaunchedEffect(Unit) {
