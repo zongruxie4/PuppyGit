5cea3ddf9d202ae7c79f6a3c71a36ead2b219867
bandeapart <dont.try.email.me@commit>
2025-05-20 22:39:55 +0800

    Diff页面选择行比较调整： 1 选择行进行比较后，避免不必要的拷贝
    2 尝试修复 DiffScreen 选择比较行之后，待比较行buffer没清空的bug，是否有效有待时间考验（经测试和以前一样，偶发布bug）

--- app/src/main/java/com/catpuppyapp/puppygit/compose/DiffRow.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/DiffRow.kt
@@ -96,7 +96,7 @@ fun DiffRow (
     setComparePairBuffer: (CompareLinePair) -> Unit,
 //    comparePair:CustomStateSaveable<CompareLinePair>,
     betterCompare:Boolean,
-    reForEachDiffContent:()->Unit,
+    reForEachDiffContent:()->Unit,  //这变量没什么用了，但暂且保留位置以免日后有用。之所以没用了是因为现在修改 pair buffer后就会拷贝元素更新list，附带刷新效果了
     indexStringPartListMap:MutableMap<String, CompareLinePairResult>,
     enableSelectCompare: Boolean,
     matchByWords:Boolean,
@@ -488,7 +488,7 @@ fun DiffRow (
 
             setComparePairBuffer(CompareLinePair())
 
-            reForEachDiffContent()
+//            reForEachDiffContent()
         }
     }
 
@@ -547,7 +547,7 @@ fun DiffRow (
 
             setComparePairBuffer(CompareLinePair())
 
-            reForEachDiffContent()
+//            reForEachDiffContent()
         }
 
     }
@@ -800,7 +800,7 @@ fun DiffRow (
                                         setComparePairBuffer(CompareLinePair())
 
                                         // re-render view
-                                        reForEachDiffContent()
+//                                        reForEachDiffContent()
                                     }
 
                                 }else {
