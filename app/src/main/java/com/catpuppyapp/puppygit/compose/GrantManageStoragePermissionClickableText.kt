da7ef759b48bea399fe68c9cb30ba5eedba1eb84
bandeapart <dont.try.email.me@commit>
2025-05-24 23:29:56 +0800

    把导入仓库弹窗提醒授权存储空间的文字改成红色

--- app/src/main/java/com/catpuppyapp/puppygit/compose/GrantManageStoragePermissionClickableText.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/GrantManageStoragePermissionClickableText.kt
@@ -4,7 +4,11 @@ import android.content.Context
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.OpenInNew
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -14,20 +18,32 @@ import com.catpuppyapp.puppygit.play.pro.R
 import com.catpuppyapp.puppygit.style.MyStyleKt
 import com.catpuppyapp.puppygit.utils.ActivityUtil
 
+private val color = MyStyleKt.TextColor.danger()
 
 @Composable
 fun GrantManageStoragePermissionClickableText(activityContext: Context) {
-    Row(modifier = Modifier
-        .padding(bottom = 15.dp)
-        .padding(horizontal = MyStyleKt.defaultHorizontalPadding)) {
-        ClickableText(
+    Row(
+        modifier = Modifier
+            .padding(bottom = 15.dp)
+            .padding(horizontal = MyStyleKt.defaultHorizontalPadding)
+            .clickable {
+                // grant permission for read/write external storage
+                ActivityUtil.getManageStoragePermissionOrShowFailedMsg(activityContext)
+            }
+        ,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
             text = stringResource(R.string.please_grant_permission_before_you_add_a_storage_path),
             overflow = TextOverflow.Visible,
             fontWeight = FontWeight.Light,
-            modifier = MyStyleKt.ClickableText.modifierNoPadding.clickable {
-                // grant permission for read/write external storage
-                ActivityUtil.getManageStoragePermissionOrShowFailedMsg(activityContext)
-            },
+            color = color,
+        )
+
+        InLineIcon(
+            icon = Icons.AutoMirrored.Filled.OpenInNew,
+            tooltipText = "",
+            iconColor = color
         )
     }
 }
