fbe5d9b9eef03f499f7b5467abe07666c9c104b3
bandeapart <dont.try.email.me@commit>
2025-06-04 22:28:51 +0800

    add comments

--- app/src/main/java/com/catpuppyapp/puppygit/utils/state/CustomStateHolder.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/utils/state/CustomStateHolder.kt
@@ -2,13 +2,14 @@ package com.catpuppyapp.puppygit.utils.state
 
 import androidx.compose.runtime.saveable.Saver
 import com.catpuppyapp.puppygit.utils.cache.Cache
-import com.catpuppyapp.puppygit.utils.getShortUUID
 
-// random是用来在某些情况下刷新数据的，不过一般用不到
-data class Holder<T>(var key: String, var data:T, var random:String= getShortUUID())
+data class Holder<T>(var key: String, var data:T)
 
+// random是用来在不改变data的情况下修改数据从而触发页面重新渲染的，不过一般用不到
+//data class Holder<T>(var key: String, var data:T, var random:String = getShortUUID())
 //fun <T> refreshState(state:MutableState<Holder<T>>) {
 ////    state.value = state.value
+//    // 改变random，但data不变，存data的容器变了，触发页面刷新
 //    state.value = state.value.copy(random = getShortUUID())
 //}
 
@@ -34,9 +35,10 @@ fun <T> getSaver():Saver<Holder<T>, String> {
     )
 }
 
-//keyTag+keyName 注意事项：1 组合起来必须唯一，不然数据会错乱；2 必须是常量，不然会造成内存泄漏，即使数据没用了依然保存在内存
+//keyTag+keyName 注意事项：1 组合起来必须唯一，不然数据会错乱；2 必须是常量，不然数据清除后key依然保存在内存会造成内存泄漏
+// keyTag+keyName must unique and both are constants, else may cause memory leak
 fun <T : Any> getHolder(keyTag:String, keyName:String, data: T):Holder<T> {
-    val holder = Holder<T>(key = genKey(keyTag, keyName), data=data, random=getShortUUID())
+    val holder = Holder<T>(key = genKey(keyTag, keyName), data = data)
     return holder
 }
 
