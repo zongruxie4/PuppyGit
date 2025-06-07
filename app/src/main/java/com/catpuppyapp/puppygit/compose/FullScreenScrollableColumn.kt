df120f2ae7ff290fa68d4c625c29fea5d19cdc43
bandeapart <dont.try.email.me@commit>
2025-06-07 14:05:06 +0800

    update FullScreenScrollableColumn: add border padding

--- app/src/main/java/com/catpuppyapp/puppygit/compose/FullScreenScrollableColumn.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/FullScreenScrollableColumn.kt
@@ -3,10 +3,12 @@ package com.catpuppyapp.puppygit.compose
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.catpuppyapp.puppygit.utils.baseVerticalScrollablePageModifier
 
 @Composable
@@ -17,6 +19,9 @@ fun FullScreenScrollableColumn(
     Column(
         modifier = Modifier
             .baseVerticalScrollablePageModifier(contentPadding, rememberScrollState())
+
+            // avoid text reached screen border
+            .padding(10.dp)
         ,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
