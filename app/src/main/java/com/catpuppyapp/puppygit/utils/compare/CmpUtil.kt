fe612bb82e31eb1f096976ab247a94ab53a17e86
bandeapart <dont.try.email.me@commit>
2025-06-08 14:44:42 +0800

    update CmpUtil: add method for roughly match

--- app/src/main/java/com/catpuppyapp/puppygit/utils/compare/CmpUtil.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/utils/compare/CmpUtil.kt
@@ -1,5 +1,6 @@
 package com.catpuppyapp.puppygit.utils.compare
 
+import com.catpuppyapp.puppygit.dto.Box
 import com.catpuppyapp.puppygit.utils.compare.param.CompareParam
 import com.catpuppyapp.puppygit.utils.compare.result.IndexModifyResult
 
@@ -44,4 +45,113 @@ object CmpUtil {
         }
     }
 
+    /**
+     * @param targetMatchCount if has these chars matched, treat startsWith or endsWith be true, else treat as false
+     */
+    fun roughlyMatch(str1NoLineBreak:String, str2NoLineBreak:String, targetMatchCount:Int = 5): Boolean {
+        if(str1NoLineBreak.isEmpty() || str2NoLineBreak.isEmpty()) {
+            return false
+        }
+
+        // long str contains part of short str
+        if(longerContainsPartOfShorter(str1NoLineBreak, str2NoLineBreak)) {
+            return true
+        }
+
+        // match starts with
+        if(matchStartsOrEndsWith(str1NoLineBreak, str2NoLineBreak, false, targetMatchCount)) {
+            return true
+        }
+
+        // match ends with
+        if(matchStartsOrEndsWith(str1NoLineBreak, str2NoLineBreak, true, targetMatchCount)) {
+            return true
+        }
+
+        return false
+    }
+
+    /**
+     * @param reverse true ends with else starts with
+     * @param targetMatchCount match how many chars as matched success, if this value greater than str1 or str2 's length after trimStarts/Ends, this method will return false as not matched
+     */
+    private fun matchStartsOrEndsWith(str1:String, str2:String, reverse: Boolean, targetMatchCount:Int): Boolean {
+        // use box as pointer to avoid value capture by closure
+        var str1Index = Box(getFirstNonBlankIndexOfStr(str1, reverse))
+        if(str1Index.value == -1){
+            return false
+        }
+        var str2Index = Box(getFirstNonBlankIndexOfStr(str2, reverse))
+        if(str2Index.value == -1){
+            return false
+        }
+
+        var matchCount = 0
+
+        val condition = if(reverse) {
+            { str1Index.value >= 0 && str2Index.value >= 0 }
+        }else {
+            { str1Index.value < str1.length && str2Index.value < str2.length }
+        }
+
+        val updateStr1Index = if(reverse) {
+            { str1Index.value-- }
+        }else {
+            { str1Index.value++ }
+        }
+
+        val updateStr2Index = if(reverse) {
+            { str2Index.value-- }
+        }else {
+            { str2Index.value++ }
+        }
+
+        while (condition()) {
+            if(str1[updateStr1Index()] == str2[updateStr2Index()]) {
+                matchCount++
+
+                if(matchCount >= targetMatchCount) {
+                    return true
+                }
+            }else {
+                break
+            }
+        }
+
+        return false
+    }
+
+    private fun getFirstNonBlankIndexOfStr(str:String, reverse: Boolean):Int {
+        val endIndex = str.length-1
+        // empty string
+        if(endIndex < 0) {
+            return -1
+        }
+
+        val idxRange = IntRange(0, endIndex).let { if(reverse) it.reversed() else it }
+        for(idx in idxRange) {
+            if(str[idx].isWhitespace().not()) {
+                return idx
+            }
+        }
+
+        return -1
+    }
+
+    private fun longerContainsPartOfShorter(str1: String, str2:String): Boolean {
+        val (longer, shorter) = if(str1.length >= str2.length) {
+            Pair(str1, str2)
+        }else {
+            Pair(str2, str1)
+        }
+
+        // get a range of center of the shorter string, e.g. shorter string is "abcdefjkl", may got range "cdefjk"
+        val center = shorter.length / 2
+        val start = (center-3).coerceAtLeast(0)
+        val end = (center+3).coerceAtMost(shorter.length)
+        val subShorter = shorter.substring(start, end)
+
+        // treat empty as not contains
+        return if(subShorter.isEmpty()) false else longer.contains(subShorter)
+    }
 }
