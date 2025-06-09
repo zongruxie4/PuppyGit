fa5e6e32403f517a64ce8d5378255390a86fa341
bandeapart <dont.try.email.me@commit>
2025-06-10 01:01:55 +0800

    add `MyHorizontalDivider`

---
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/MyDivider.kt
@@ -0,0 +1,22 @@
+package com.catpuppyapp.puppygit.compose
+
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+
+
+@Composable
+fun MyHorizontalDivider(
+    modifier: Modifier = Modifier,
+    thickness: Dp = DividerDefaults.Thickness,
+    color: Color = DividerDefaults.color.copy(alpha = 0.5f),
+) {
+    HorizontalDivider(
+        modifier = modifier,
+        thickness = thickness,
+        color = color,
+    )
+}
