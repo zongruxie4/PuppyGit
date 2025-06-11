405cfa6c1580d39270da12fe98a34f39c9021bbc
bandeapart <dont.try.email.me@commit>
2025-06-11 19:36:12 +0800

    submodule list, use icon instead of text when no submodules

--- app/src/main/java/com/catpuppyapp/puppygit/screen/SubmoduleListScreen.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/screen/SubmoduleListScreen.kt
@@ -2,15 +2,12 @@ package com.catpuppyapp.puppygit.screen
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -34,7 +31,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -48,7 +44,6 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.catpuppyapp.puppygit.compose.BottomBar
-import com.catpuppyapp.puppygit.compose.ClickableText
 import com.catpuppyapp.puppygit.compose.ConfirmDialog2
 import com.catpuppyapp.puppygit.compose.CopyableDialog
 import com.catpuppyapp.puppygit.compose.CredentialSelector
@@ -1151,19 +1146,11 @@ fun SubmoduleListScreen(
                             Text(text = stringResource(R.string.no_submodules_found))
                         }
 
-                        Row(
-                            modifier = Modifier.padding(top = 10.dp),
-                            horizontalArrangement = Arrangement.Center,
-                            verticalAlignment = Alignment.CenterVertically
+                        LongPressAbleIconBtn(
+                            icon = Icons.Filled.Add,
+                            tooltipText = stringResource(R.string.create),
                         ) {
-                            ClickableText(
-                                text =  stringResource(R.string.create),
-                                modifier = MyStyleKt.ClickableText.modifierNoPadding
-                                    .clickable {
-                                        initCreateDialog()
-                                    }
-                                ,
-                            )
+                            initCreateDialog()
                         }
                     }
                 }
