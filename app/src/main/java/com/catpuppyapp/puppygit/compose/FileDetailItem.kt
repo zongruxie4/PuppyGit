acdd70013acb7cc839b9aa446ee579e8dcf17298
bandeapart <dont.try.email.me@commit>
2025-06-11 23:01:17 +0800

    update Editor recent list view: adjust color

--- app/src/main/java/com/catpuppyapp/puppygit/compose/FileDetailItem.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/FileDetailItem.kt
@@ -17,7 +17,13 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.catpuppyapp.puppygit.dto.FileDetail
 import com.catpuppyapp.puppygit.style.MyStyleKt
-import com.catpuppyapp.puppygit.utils.UIHelper
+import com.catpuppyapp.puppygit.ui.theme.Theme
+
+
+private val bgColorInDarkTheme = Color(0xFF545454)
+private val contentTextColorInDarkTheme = Color(0xFFA8A8A8)
+private val bgColorInLightTheme = Color(0xD8E5E5E5)
+private val contentTextColorInLightTheme = Color(0xFF595959)
 
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -31,10 +37,12 @@ fun FileDetailItem(
     onLongClick:(idx:Int, FileDetail)->Unit,
     onClick:(FileDetail)->Unit,
 ){
+    val inDarkTheme = Theme.inDarkTheme
+
     Column(
         modifier = Modifier
             .padding(margin)
-            .background(if(selected) MaterialTheme.colorScheme.primaryContainer else UIHelper.defaultCardColor())
+            .background(if(selected) MaterialTheme.colorScheme.primaryContainer else if(inDarkTheme) bgColorInDarkTheme else bgColorInLightTheme)
             .combinedClickable(
                 onLongClick = { onLongClick(idx, item) },
             ) {
@@ -53,16 +61,16 @@ fun FileDetailItem(
                 Text(item.file.name, fontSize = MyStyleKt.Title.firstLineFontSize, color = fontColor, fontWeight = FontWeight.Bold)
             }
             ScrollableRow {
-                Text(item.file.path.ioPath, fontSize = MyStyleKt.Title.secondLineFontSize, color = fontColor)
+                Text(item.file.path.ioPath, fontSize = MyStyleKt.Title.secondLineFontSize, color = fontColor, fontWeight = FontWeight.Light)
             }
         }
 
         MyHorizontalDivider()
 
         Row(
-            modifier = Modifier.padding(10.dp)
+            modifier = Modifier.padding(5.dp)
         ) {
-            Text(item.shortContent, color = fontColor)
+            Text(item.shortContent, color = if(inDarkTheme) contentTextColorInDarkTheme else contentTextColorInLightTheme, fontWeight = FontWeight.Light)
         }
     }
 }
