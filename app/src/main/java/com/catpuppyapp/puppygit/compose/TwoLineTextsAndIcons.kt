649a6ad293173329f22bd8555713d7dc00d9e66e
bandeapart <dont.try.email.me@commit>
2025-06-13 18:12:54 +0800

    adjust style of `TwoLineTextsAndIcons` (Automation screen selected unselected used this component)

--- app/src/main/java/com/catpuppyapp/puppygit/compose/TwoLineTextsAndIcons.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/TwoLineTextsAndIcons.kt
@@ -1,16 +1,19 @@
 package com.catpuppyapp.puppygit.compose
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.catpuppyapp.puppygit.style.MyStyleKt
 
 
 @Composable
@@ -33,12 +36,16 @@ fun TwoLineTextsAndIcons(
                 .fillMaxWidth(.7f),
             verticalArrangement = Arrangement.Center,
         ) {
-            MySelectionContainer {
-                Text(text = text1)
+            SelectionRow(
+                Modifier.horizontalScroll(rememberScrollState())
+            ) {
+                Text(text = text1, fontSize = MyStyleKt.Title.firstLineFontSize, fontWeight = FontWeight.Bold)
             }
 
-            MySelectionContainer {
-                Text(text = text2, style = MaterialTheme.typography.bodySmall)
+            SelectionRow(
+                Modifier.horizontalScroll(rememberScrollState())
+            ) {
+                Text(text = text2, fontSize = MyStyleKt.Title.secondLineFontSize, fontWeight = FontWeight.Light)
             }
         }
 
