aeaf83b5ef4c2115e9e324843deafa13625f86c8
bandeapart <dont.try.email.me@commit>
2025-06-15 18:57:14 +0800

    update TextEditor `onKeyEvent`

--- app/src/main/java/com/catpuppyapp/puppygit/fileeditor/texteditor/view/TextEditor.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/fileeditor/texteditor/view/TextEditor.kt
@@ -42,10 +42,12 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.isCtrlPressed
 import androidx.compose.ui.input.key.isShiftPressed
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalHapticFeedback
@@ -1128,6 +1130,9 @@ fun TextEditor(
                                 }
                                 .onKeyEvent { keyEvent ->
                                     // return true to stop key event propaganda
+                                    if(keyEvent.type != KeyEventType.KeyDown) {
+                                        return@onKeyEvent false
+                                    }
 
                                     if (keyEvent.isCtrlPressed && keyEvent.key == Key.S) { // save
                                         requestFromParent.value = PageRequest.requireSave
