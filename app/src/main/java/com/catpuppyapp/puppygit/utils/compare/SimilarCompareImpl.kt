20a0b74d4a9e2ff7b92cc0d6b58dbd109cfec021
bandeapart <dont.try.email.me@commit>
2025-06-26 11:12:20 +0800

    optimize compare algorithm: guess is prepend text or append text, if prepend, reverse match punctuations to improve matched rate

--- app/src/main/java/com/catpuppyapp/puppygit/utils/compare/SimilarCompareImpl.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/utils/compare/SimilarCompareImpl.kt
@@ -7,6 +7,7 @@ import com.catpuppyapp.puppygit.utils.compare.result.IndexStringPart
 import com.catpuppyapp.puppygit.utils.compare.search.Search
 import com.catpuppyapp.puppygit.utils.compare.search.SearchDirection
 import com.catpuppyapp.puppygit.utils.iterator.NoCopyIterator
+import com.catpuppyapp.puppygit.utils.iterator.ReverseNoCopyIterator
 
 
 // 这里不要把泛型写到class上，除非class里有字段是泛型的类型，否则还是写到函数上更好，这样可以用一个类实例处理不同类型的参数，若写类上，得为不同参数创建不同的类实例，麻烦。
@@ -160,8 +161,8 @@ class SimilarCompareImpl: SimilarCompare {
         }
 
 
-        val addSpaceIter = NoCopyIterator(srcList = addWordSpacePair.spaces as MutableList)
-        val delSpaceIter = NoCopyIterator(srcList = delWordSpacePair.spaces as MutableList)
+        val addSpaceIter = ReverseNoCopyIterator(srcList = addWordSpacePair.spaces as MutableList)
+        val delSpaceIter = ReverseNoCopyIterator(srcList = delWordSpacePair.spaces as MutableList)
 
         // match spaces by equals
         matched = doEqualsMatchWordsOrSpaces(addSpaceIter, delSpaceIter, addIndexResultList, delIndexResultList) || matched
--- app/src/main/java/com/catpuppyapp/puppygit/utils/iterator/NoCopyIterator.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/utils/iterator/NoCopyIterator.kt
@@ -1,33 +1,64 @@
 package com.catpuppyapp.puppygit.utils.iterator
 
 /**
- * no copy, no tread safe iterator
- * 无拷贝，线程不安全的iterator
+ * no copy, no tread safe iterator, but faster
+ * 无拷贝，线程不安全的iterator，但是快
  * @author Bandeapart1964 of catpuppyapp
  */
-class NoCopyIterator<T>(
+open class NoCopyIterator<T>(
     val srcList:MutableList<T>
 ):MutableIterator<T> {
 
-    var count:Int=0
+    /**
+     * indicate the iteration order is reversed or not
+     */
+    open val isReversed = false
 
-    fun reset(){
-        count = 0
+    protected open var currentIndex:Int = 0
+
+    open fun reset(){
+        currentIndex = 0
     }
 
-    fun srcIsEmpty():Boolean {
+    open fun srcIsEmpty():Boolean {
         return srcList.isEmpty()
     }
 
+
     override fun hasNext(): Boolean {
-        return count < srcList.size
+        return currentIndex >= 0 && currentIndex < srcList.size
     }
 
     override fun next(): T {
-        return srcList[count++]
+        return srcList[currentIndex++]
     }
 
     override fun remove() {
-        srcList.removeAt(--count)
+        srcList.removeAt(--currentIndex)
+    }
+}
+
+class ReverseNoCopyIterator<T>(
+    srcList:MutableList<T>
+): NoCopyIterator<T>(srcList) {
+
+    override val isReversed = true
+
+    override var currentIndex:Int = srcList.size - 1
+
+    override fun reset(){
+        currentIndex = srcList.size - 1
+    }
+
+    override fun srcIsEmpty():Boolean {
+        return srcList.isEmpty()
+    }
+
+    override fun next(): T {
+        return srcList[currentIndex--]
+    }
+
+    override fun remove() {
+        srcList.removeAt(++currentIndex)
     }
 }
