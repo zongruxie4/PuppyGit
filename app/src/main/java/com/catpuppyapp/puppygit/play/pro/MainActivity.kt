60aab35d8ae9b18c646275bcc8bf05a6f37379c6
bandeapart <dont.try.email.me@commit>
2025-06-28 18:55:05 +0800

    把捕获compose异常的方法抽到base activity里

--- app/src/main/java/com/catpuppyapp/puppygit/play/pro/MainActivity.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/play/pro/MainActivity.kt
@@ -130,37 +130,7 @@ class MainActivity : BaseComposeActivity() {
         //for make imePadding() work
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        // for catch exception, block start。( refer: https://stackoverflow.com/questions/76061623/how-to-restart-looper-when-exception-throwed-in-jetpack-compose
-        val recomposer = window.decorView.createLifecycleAwareWindowRecomposer(
-            CoroutineExceptionHandler { coroutineContext, throwable ->
-                try {
-    //                throwable.printStackTrace();
-                    val errMsg = throwable.stackTraceToString()
-                    MyLog.e(TAG, "#$funName err: $errMsg")
-
-                    //出错提示下用户就行，经我测试，画面会冻结，但数据不会丢，问题不大
-                    showToast(applicationContext, getString(R.string.err_restart_app_may_resolve), Toast.LENGTH_LONG)  //测试了下，能显示Toast
-
-                    //不重新创建Activity的话，页面会freeze，按什么都没响应，不过系统导航键还是可用的
-                    //重新创建不一定成功，有可能会返回桌面
-//                    ActivityUtil.restartActivityByIntent(this)
-
-                    //启动crash activity显示错误信息
-                    CrashActivity.start(this, errMsg)
-
-                    // 不重建当前Activity，直接退出
-                    finish()
-
-                    // 如果想显示错误弹窗，参见文档 “下一步-20240120.txt” 中的："compose错误处理 compose出错弹窗实现 20240505"
-
-                }catch (e:Exception) {
-                    e.printStackTrace()  //再出异常，管不了了，随便吧，打印下就行
-                }
-            }, lifecycle)
-
-        // set window use our recomposer
-        window.decorView.compositionContext = recomposer
-        // for catch exception, block end
+        setExceptionHandler(TAG, funName)
 
 //        // val settings = SettingsUtil.getSettingsSnapshot()
 
@@ -172,6 +142,8 @@ class MainActivity : BaseComposeActivity() {
         }
 
     }
+
+
 
     override fun onNewIntent(intent: Intent) {
         MyLog.d(TAG, "#onNewIntent() called")
--- app/src/main/java/com/catpuppyapp/puppygit/play/pro/base/BaseComposeActivity.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/play/pro/base/BaseComposeActivity.kt
@@ -2,9 +2,17 @@ package com.catpuppyapp.puppygit.play.pro.base
 
 import android.content.Context
 import android.content.res.Configuration
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.compositionContext
+import androidx.compose.ui.platform.createLifecycleAwareWindowRecomposer
+import com.catpuppyapp.puppygit.play.pro.CrashActivity
+import com.catpuppyapp.puppygit.play.pro.R
 import com.catpuppyapp.puppygit.utils.AppModel
 import com.catpuppyapp.puppygit.utils.ContextUtil
+import com.catpuppyapp.puppygit.utils.MyLog
+import com.catpuppyapp.puppygit.utils.showToast
+import kotlinx.coroutines.CoroutineExceptionHandler
 
 open class BaseComposeActivity : ComponentActivity() {
 
@@ -29,4 +37,41 @@ open class BaseComposeActivity : ComponentActivity() {
 
         AppModel.updateExitApp { finish() }
     }
+
+    /**
+     * can't promise 100% catch app crash, but have maybe 30% chance to caught
+     */
+    protected fun setExceptionHandler(TAG: String, funName: String) {
+        // catch exception, block start。( refer: https://stackoverflow.com/questions/76061623/how-to-restart-looper-when-exception-throwed-in-jetpack-compose
+        window.decorView.compositionContext = window.decorView.createLifecycleAwareWindowRecomposer(
+            CoroutineExceptionHandler { coroutineContext, throwable ->
+                try {
+                    //                throwable.printStackTrace();
+                    val errMsg = throwable.stackTraceToString()
+                    MyLog.e(TAG, "#$funName err: $errMsg")
+
+                    //出错提示下用户就行，经我测试，画面会冻结，但数据不会丢，问题不大
+                    showToast(applicationContext, getString(R.string.err_restart_app_may_resolve), Toast.LENGTH_LONG)  //测试了下，能显示Toast
+
+                    //不重新创建Activity的话，页面会freeze，按什么都没响应，不过系统导航键还是可用的
+                    //重新创建不一定成功，有可能会返回桌面
+                    //                    ActivityUtil.restartActivityByIntent(this)
+
+                    //启动crash activity显示错误信息
+                    CrashActivity.start(this, errMsg)
+
+                    // 不重建当前Activity，直接退出
+                    finish()
+
+                    // 如果想显示错误弹窗，参见文档 “下一步-20240120.txt” 中的："compose错误处理 compose出错弹窗实现 20240505"
+
+                } catch (e: Exception) {
+                    e.printStackTrace()  //再出异常，管不了了，随便吧，打印下就行
+                }
+            }, lifecycle
+        )
+
+        // catch exception, block end
+    }
+
 }
