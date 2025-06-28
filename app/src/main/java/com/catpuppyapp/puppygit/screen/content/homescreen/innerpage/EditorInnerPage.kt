d73ec81d9d17e40444a5aa6132b8cb52e719fd28
bandeapart <dont.try.email.me@commit>
2025-06-28 13:20:22 +0800

    update `EditorInnerPage`: add contentPadding for sora editor

--- app/src/main/java/com/catpuppyapp/puppygit/screen/content/homescreen/innerpage/EditorInnerPage.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/screen/content/homescreen/innerpage/EditorInnerPage.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridState
 import androidx.compose.material.icons.Icons
@@ -1664,6 +1665,7 @@ fun EditorInnerPage(
     if(soraEditorTestPassed && isTimeShowEditor) {
         CodeEditor(
             modifier = Modifier
+                .padding(contentPadding)
                 .fillMaxSize(),
             state = rememberCodeEditorState()
         )
