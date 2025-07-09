a62407fdd938d90ccd0dd94b47326771c741b4e9
bandeapart <dont.try.email.me@commit>
2025-07-10 00:39:21 +0800

    Done: editor syntax highlighting: 添加 go和cpp语法高亮

--- app/src/main/java/com/catpuppyapp/puppygit/codeeditor/PLScopes.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/codeeditor/PLScopes.kt
@@ -9,24 +9,45 @@ object PLScopes {
     // init value for state
     val AUTO = "AUTO_DETECTED"
 
+    // order by a-z
+    val CPP = "source.cpp"
+    val GO = "source.go"
     val HTML = "text.html.basic"
     val JAVA = "source.java"
+    val KOTLIN = "source.kotlin"
 
     val SCOPES = listOf(
         NONE,
+        CPP,
+        GO,
         HTML,
         JAVA,
+        KOTLIN,
 
     )
 
 
     fun guessScope(fileName: String):String {
+        val fileName = fileName.lowercase()
+
         if(fileName.endsWith(".html") || fileName.endsWith(".htm")) {
             return HTML
         }
 
         if(fileName.endsWith(".java")) {
             return JAVA
+        }
+
+        if(fileName.endsWith(".kotlin") || fileName.endsWith(".kt")) {
+            return KOTLIN
+        }
+
+        if(fileName.endsWith(".go")) {
+            return GO
+        }
+
+        if(fileName.endsWith(".cpp") || fileName.endsWith(".cc") || fileName.endsWith(".cxx")) {
+            return CPP
         }
 
         return NONE
