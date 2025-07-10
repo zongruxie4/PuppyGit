758c22439fed96a2e55e04e923faf63ac251d596
bandeapart <dont.try.email.me@commit>
2025-07-10 14:59:35 +0800

    update `PLScopes`: add more file name extensions check

--- app/src/main/java/com/catpuppyapp/puppygit/codeeditor/PLScopes.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/codeeditor/PLScopes.kt
@@ -30,10 +30,10 @@ enum class PLScopes(val scope: String) {
     KOTLIN("source.kotlin"),
     LESS("source.css.less"),
     LUA("source.lua"),
+    MAKE_FILE("source.makefile"),
     MARKDOWN("text.html.markdown"),
     MARKDOWN_MATH("text.html.markdown.math"),
     PERL("source.perl"),
-    MAKE_FILE("source.makefile"),
     PHP("source.php"),
     POWER_SHELL("source.powershell"),
     PYTHON("source.python"),
@@ -47,8 +47,9 @@ enum class PLScopes(val scope: String) {
     XML("text.xml"),
     YAML("text.yaml"),
 
-
     ;
+
+
     companion object {
 
         val SCOPES = PLScopes.entries.map { it.scope }
@@ -70,6 +71,26 @@ enum class PLScopes(val scope: String) {
                 return JAVA.scope
             }
 
+            if(fileName.endsWith(".js") || fileName.endsWith(".javascript")) {
+                return JAVASCRIPT.scope
+            }
+
+            if(fileName.endsWith(".jsx")) {
+                return JSX.scope
+            }
+
+            if(fileName.endsWith(".json")) {
+                return JSON.scope
+            }
+
+            if(fileName.endsWith(".less")) {
+                return LESS.scope
+            }
+
+            if(fileName.endsWith(".lua")) {
+                return LUA.scope
+            }
+
             if(fileName.endsWith(".kotlin") || fileName.endsWith(".kt")) {
                 return KOTLIN.scope
             }
@@ -82,6 +103,10 @@ enum class PLScopes(val scope: String) {
                 return CPP.scope
             }
 
+            if(fileName.endsWith(".c")) {
+                return C.scope
+            }
+
 
             if(fileName.endsWith(".diff") || fileName.endsWith(".patch")) {
                 return DIFF.scope
@@ -92,16 +117,81 @@ enum class PLScopes(val scope: String) {
                 return BAT.scope
             }
 
-            if(fileName.endsWith(".c")) {
-                return C.scope
-            }
-
             if(fileName.endsWith(".cs")) {
                 return CSHARP.scope
             }
 
-            if(fileName.endsWith(".mdmath") || fileName.endsWith(".mdm")) {
+            if(fileName.endsWith(".css")) {
+                return CSS.scope
+            }
+
+            if(fileName.endsWith(".dart")) {
+                return DART.scope
+            }
+
+            if(fileName == "dockerfile" || fileName.endsWith(".dockerfile")) {
+                return DOCKER_FILE.scope
+            }
+
+            if(fileName.endsWith(".mdmath") || fileName.endsWith(".mdm") || fileName.endsWith(".mmd")) {
                 return MARKDOWN_MATH.scope
+            }
+
+
+            if(fileName.endsWith(".perl")) {
+                return PERL.scope
+            }
+
+            if(fileName.endsWith(".makefile")) {
+                return MAKE_FILE.scope
+            }
+
+            if(fileName.endsWith(".php")) {
+                return PHP.scope
+            }
+
+            if(fileName.endsWith(".ps1")) {
+                return POWER_SHELL.scope
+            }
+
+            if(fileName.endsWith(".py") || fileName.endsWith(".python")) {
+                return PYTHON.scope
+            }
+
+            if(fileName.endsWith(".rb") || fileName.endsWith(".ruby")) {
+                return RUBY.scope
+            }
+
+            if(fileName.endsWith(".rust")) {
+                return RUST.scope
+            }
+
+            if(fileName.endsWith(".scss") || fileName.endsWith(".sass")) {
+                return SCSS.scope
+            }
+
+            if(fileName.endsWith(".sh")) {
+                return SHELL.scope
+            }
+
+            if(fileName.endsWith(".sql")) {
+                return SQL.scope
+            }
+
+            if(fileName.endsWith(".swift")) {
+                return SWIFT.scope
+            }
+
+            if(fileName.endsWith(".ts")) {
+                return TS.scope
+            }
+
+            if(fileName.endsWith(".xml")) {
+                return XML.scope
+            }
+
+            if(fileName.endsWith(".yml") || fileName.endsWith(".yaml")) {
+                return YAML.scope
             }
 
 
