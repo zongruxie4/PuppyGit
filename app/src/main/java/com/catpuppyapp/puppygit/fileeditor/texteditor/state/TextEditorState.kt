1dc4fbef732a0e1da7a63fd25e534e409a2a241e
bandeapart <dont.try.email.me@commit>
2025-07-11 01:50:04 +0800

    add logs

--- app/src/main/java/com/catpuppyapp/puppygit/fileeditor/texteditor/state/TextEditorState.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/fileeditor/texteditor/state/TextEditorState.kt
@@ -2308,6 +2308,7 @@ class TextEditorState private constructor(
 
         // style will update spans
         stylesResult.styles.adjustOnDelete(start, end)
+        MyLog.d(TAG, "#$funName: adjusted on delete, spans count = ${stylesResult.styles.spans.lineCount}")
 
 
         val selectedText = getSelectedText(IntRange(startLineIndex, endLineIndexInclusive).toList(), keepEndLineBreak = !isDelLastLine)
@@ -2390,6 +2391,7 @@ class TextEditorState private constructor(
 
         // style will update spans
         stylesResult.styles.adjustOnInsert(start, end)
+        MyLog.d(TAG, "#$funName: adjusted on insert, spans count = ${stylesResult.styles.spans.lineCount}")
 
         val selectedText = insertedContent
         val lang = codeEditor?.myLang
