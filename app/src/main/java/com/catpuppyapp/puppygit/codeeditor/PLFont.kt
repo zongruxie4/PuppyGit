55a7eb2677066fc6f3dd195cd57622e50b6f308e
bandeapart <dont.try.email.me@commit>
2025-07-13 10:47:01 +0800

    use no-ligature font as bundled font

--- app/src/main/java/com/catpuppyapp/puppygit/codeeditor/PLFont.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/codeeditor/PLFont.kt
@@ -6,7 +6,9 @@ import com.catpuppyapp.puppygit.play.pro.R
 import com.catpuppyapp.puppygit.settings.SettingsUtil
 
 object PLFont {
-    val codeFont = FontFamily(Font(R.font.jb_mono_regular))
+    // nl 是 no-ligature，非连体字
+    // nl is no-ligature
+    val codeFont = FontFamily(Font(R.font.jb_mono_nl_regular))
 
     fun editorCodeFont() = if(SettingsUtil.isEditorUseSystemFonts()) null else codeFont
 
