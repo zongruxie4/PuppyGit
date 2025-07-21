dc5aa73567c6f5c5dd11eea476a40c99a04a70cd
bandeapart <dont.try.email.me@commit>
2025-07-21 12:33:47 +0800

    WIP: diff screen syntax: Dirty Code, fix syntax highlighting style lost after refresh

--- app/src/main/java/com/catpuppyapp/puppygit/compose/DiffRow.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/DiffRow.kt
@@ -657,7 +657,7 @@ fun DiffRow (
             )
         }
 
-        val stylePartList = diffItemSaver.operateStylesMapWithReadLock { it.get(line.key) }
+        val obtainStylePartList = { diffItemSaver.operateStylesMapWithReadLock { it.get(line.key) } }
 
         val contentModifier = Modifier
             // if fill max width, will not be able to saw the spaces at the end, because the background will fill the whole line;
@@ -681,7 +681,7 @@ fun DiffRow (
                 //注意：这里不能改成用多个Text组件，不然若超过屏幕宽度软换行会失效
                 Text(
                     text = buildAnnotatedString {
-                        stylePartList?.let { stylePartList ->
+                        obtainStylePartList()?.let { stylePartList ->
                             PuppyLine.mergeStringAndStylePartList(stringPartList, stylePartList, bgColor).forEachBetter {
                                 withStyle(it.style) {
                                     append(content.substring(it.range))
@@ -714,7 +714,7 @@ fun DiffRow (
                 //文本内容
                 Text(
                     text = buildAnnotatedString {
-                        stylePartList?.forEachBetter {
+                        obtainStylePartList()?.forEachBetter {
                             withStyle(it.style) {
                                 append(content.substring(it.range))
                             }
