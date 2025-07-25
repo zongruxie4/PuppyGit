0a045884a1893618bf4a6c590a9d183db6394aac
bandeapart <dont.try.email.me@commit>
2025-07-25 14:53:25 +0800

    update 'gradle / room / ksp' plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val kotlinVersion="2.1.10"
+    val kotlinVersion = "2.1.10"
     val androidVersion = "8.11.1"
 
     id("com.android.application") version androidVersion apply false
@@ -8,8 +8,8 @@ plugins {
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion apply false
-    id("com.google.devtools.ksp") version "2.1.10-1.0.29" apply false
-    id("androidx.room") version "2.7.1" apply false
+    id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
+    id("androidx.room") version "2.7.2" apply false
 
 //    id("io.ktor.plugin") version "3.0.3" apply false
 
