c699f37608d6be6dc13d0f10859abae920609667
bandeapart <dont.try.email.me@commit>
2025-07-25 15:18:01 +0800

    update kotlin and ksp plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val kotlinVersion = "2.1.10"
+    val kotlinVersion = "2.2.0"
     val androidVersion = "8.11.1"
 
     id("com.android.application") version androidVersion apply false
@@ -8,9 +8,10 @@ plugins {
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion apply false
-    id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
     id("androidx.room") version "2.7.2" apply false
 
+    // not compatible to android
 //    id("io.ktor.plugin") version "3.0.3" apply false
 
 }
