d2f9957028b484dd3eb3f0d2914d1aa75c34c2e5
bandeapart <dont.try.email.me@commit>
2025-07-28 00:06:21 +0800

    remove unused animation scroll

--- app/src/main/java/com/catpuppyapp/puppygit/compose/BottomBar.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/BottomBar.kt
@@ -266,7 +266,7 @@ fun BottomBar(
                         // trying scroll to the most right of the list
                         //  200 is a guessed number, any number is ok,
                         //  just make sure it big enough to reach the end of the list
-                        UIHelper.scrollTo(scope, scrollableIconListState, 200 * iconTextList.size, animation = true)
+                        UIHelper.scrollTo(scope, scrollableIconListState, 200 * iconTextList.size)
                     }
                 }
 
