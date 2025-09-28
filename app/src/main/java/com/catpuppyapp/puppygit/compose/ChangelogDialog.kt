a81e6532814aa509a8c885a7b23e79ddb68a1cd7
bandeapart <dont.try.email.me@commit>
2025-09-28 12:49:36 +0800

    adjust style of `ChangelogDialog`

--- app/src/main/java/com/catpuppyapp/puppygit/compose/ChangelogDialog.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/compose/ChangelogDialog.kt
@@ -5,28 +5,31 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.LinkAnnotation
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withLink
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.catpuppyapp.puppygit.screen.content.homescreen.innerpage.donateLink
-import com.catpuppyapp.puppygit.style.MyStyleKt
+import com.catpuppyapp.puppygit.utils.ActivityUtil
 
+
+private const val version = "1.1.4.2v118"
 
 private val changelog = """
-    1.1.4.2v118:
     - Make text of dialog copyable 
     - Add changelog dialog
     - 使弹窗文字可拷贝
     - 添加更新日志弹窗
 """.trimIndent()
 
+
+private const val donateText = "❤ Donate to support develop! ❤"
+
+
 @Composable
 fun ChangelogDialog(
     onClose: () -> Unit,
 ) {
+    val activityContext = LocalContext.current
+
     CopyableDialog2(
         // hide ok btn
         okCompose = {},
@@ -35,19 +38,13 @@ fun ChangelogDialog(
 
         onCancel = onClose,
 
-        title = "Changelog",
+        title = version,
         requireShowTextCompose = true,
         textCompose = {
             ScrollableColumn {
-                Text(
-                    text = buildAnnotatedString {
-                        withLink(LinkAnnotation.Url(donateLink)) {
-                            withStyle(style = SpanStyle(fontWeight = MyStyleKt.TextItem.defaultFontWeight(), color = MyStyleKt.ClickableText.getColor(), fontSize = MyStyleKt.ClickableText.fontSize)) {
-                                append("❤ Donate to support develop! ❤")
-                            }
-                        }
-                    }
-                )
+                MultiLineClickableText(donateText) {
+                    ActivityUtil.openUrl(activityContext, donateLink)
+                }
 
                 Spacer(Modifier.height(15.dp))
 
