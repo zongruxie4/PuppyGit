23fb9ed4b4ee053a207d33951476ccf683b56166
bandeapart <dont.try.email.me@commit>
2026-03-11 18:44:23 +0800

    add contributors: jiesou and clach04

--- app/src/main/java/com/catpuppyapp/puppygit/screen/content/homescreen/innerpage/AboutInnerPage.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/screen/content/homescreen/innerpage/AboutInnerPage.kt
@@ -108,6 +108,8 @@ private val contributorList = listOf(
     Contributor(name = "kamilhussen24", link = "https://github.com/kamilhussen24", desc = "Bangla translator"),
     Contributor(name = "OutlinedArc217", link = "https://github.com/OutlinedArc217", desc = "UI improvement"),
     Contributor(name = "RokeJulianLockhart", link = "https://github.com/RokeJulianLockhart", desc = "UI improvement"),
+    Contributor(name = "jiesou", link = "https://github.com/jiesou", desc = "Fix automation service crash"),
+    Contributor(name = "clach04", link = "https://github.com/clach04", desc = "Optimize english translation"),
 )
 
 
