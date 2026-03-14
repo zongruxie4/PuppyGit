da272556c7fcf72e465d7b357d6136ed474f4d94
bandeapart <dont.try.email.me@commit>
2026-03-14 14:36:42 +0800

    remove all upper case A-Z from random string seed

--- app/src/main/java/com/catpuppyapp/puppygit/utils/Utils.kt
+++ app/src/main/java/com/catpuppyapp/puppygit/utils/Utils.kt
@@ -204,7 +204,8 @@ fun getShortUUID(len:Int=20):String {
 
 
 // start: 生成随机字符串，字符集：a..z, 0..9, A..Z
-val randomStringCharList = listOf('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+//val randomStringCharList = listOf('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+val randomStringCharList = listOf('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9');
 
 /**
  * 生成随机字符串比生成真UUID性能更好，好近10倍
