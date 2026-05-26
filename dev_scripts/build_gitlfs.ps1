386e1514b3246d2a027d24188a14cf45104dbd69
Bandeapart1964 <noreplay.47898432@github.com>
2026-05-26 10:29:25 +0800

    更新build lfs命令

--- dev_scripts/build_gitlfs.ps1
+++ dev_scripts/build_gitlfs.ps1
@@ -8,7 +8,10 @@ $env:CGO_ENABLED="0"
 $env:GOOS="android"
 $env:GOARCH="arm64"
 
-go build -v -ldflags="-s -w" -o bin/git-lfs-android-arm64
+# -trimpath二进制文件中避免记录绝对路径
+# -buildvcs=false 避免记录git信息，没用
+# buildid=空字符串，有助于重复构建生成相同二进制文件，ai说的，未考证
+go build -v -trimpath -buildvcs=false -ldflags="-s -w -buildid=" -o bin/git-lfs-android-arm64
 
 echo "arm64 done"
 
@@ -20,7 +23,7 @@ $env:GOOS = "android"
 $env:GOARCH = "arm"
 $env:CC = "$ndkToolchains\\llvm\\prebuilt\\windows-x86_64\\bin\\armv7a-linux-androideabi21-clang.cmd"
 
-go build -ldflags="-s -w" -o bin/git-lfs-android-arm
+go build -v -trimpath -buildvcs=false -ldflags="-s -w -buildid=" -o bin/git-lfs-android-arm
 
 echo "arm done"
 
@@ -31,7 +34,7 @@ $env:GOOS = "android"
 $env:GOARCH = "amd64"
 $env:CC = "$ndkToolchains\\llvm\\prebuilt\\windows-x86_64\\bin\\x86_64-linux-android21-clang.cmd"
 
-go build -ldflags="-s -w" -o bin/git-lfs-android-x8664
+go build -v -trimpath -buildvcs=false -ldflags="-s -w -buildid=" -o bin/git-lfs-android-x8664
 
 echo "x8664 done"
 
@@ -42,7 +45,7 @@ $env:GOOS = "android"
 $env:GOARCH = "386"
 $env:CC = "$ndkToolchains\\llvm\\prebuilt\\windows-x86_64\\bin\\i686-linux-android21-clang.cmd"
 
-go build -ldflags="-s -w" -o bin/git-lfs-android-x86
+go build -v -trimpath -buildvcs=false -ldflags="-s -w -buildid=" -o bin/git-lfs-android-x86
 
 echo "x86 done"
 
