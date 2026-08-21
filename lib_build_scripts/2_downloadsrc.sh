f6f273ab204227fd6288862ceb97ba35809c7d70
bandeapart <dont.try.email.me@commit>
2026-08-21 13:35:30 +0800

    update openssl to 4.0.1; update libgit2 to 1.9.7

--- lib_build_scripts/2_downloadsrc.sh
+++ lib_build_scripts/2_downloadsrc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # set version and download link, make sure download file is tar.gz, else need edit the uncompress command
 LIBSSH2_VERSION="1.11.1"
-OPENSSL_VERSION="4.0.0"
-LIBGIT2_VERSION="1.9.4"
+OPENSSL_VERSION="4.0.1"
+LIBGIT2_VERSION="1.9.7"
 
 
 
